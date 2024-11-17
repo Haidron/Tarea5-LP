d6443aed34f7ddbec974bb17e5d2162f048e5b49
Haidron <nicolazignacio21@gmail.com>
2024-11-17 15:59:25 -0300

    tarea terminada

--- p1.pl
+++ p1.pl
@@ -1,10 +1,24 @@
+% Predicado principal que verifica si una sublista dentro de una lista es es palindroma, utiliza el predicado sublista/4 para extraer la sublista
+% y utliza el predicado es_palindromo/1 para realizar la verificacion una vez que ya se ha extraido la sublista
+% Parametros:
+% - Lista: lista de caracteres que se utilizara
+% - [L, R]: son los limites de la sublista que se verificará 
 esPalindroma(Lista, [L, R]) :-
     sublista(Lista, L, R, SubSegmento),
     es_palindromo(SubSegmento).
 
+% Predicado para la sublista de una lista dada
+% Parametros:
+% - Lista: lista de caracteres que se utilizara
+% - L: indice de la lista que sera el inicio de la sublista
+% - R: indice de la lista que sera el final de la sublista
+% - SubSegmento: es la sublista resultante
 sublista(Lista, L, R, SubSegmento) :-
     findall(Elem, (nth1(Index, Lista, Elem), Index >= L, Index =< R), SubSegmento).
 
 
+% Predicado para verificar si una lista es un palindromo, a este predicado solo se le proveera una sublista como parametro
+% Parametros:
+% - Lista: La lista a verificar
 es_palindromo(Lista) :-
     reverse(Lista, Lista).
--- p2.pl
+++ p2.pl
@@ -17,34 +17,54 @@ puente(p6, p2, 2).
 puente(p7, p2, 3).
 puente(p2, p3, 7).
 
+% Predicado que encuentra y muestra todas las rutas desde el planeta S hasta el centro galáctico c.
+% Utiliza el predicado buscar_camino/4 para encontrar caminos invertidos, los invierte y los imprime.
+% Parametros:
+% - S: Nodo de inicio (planeta de origen).
+% - Res: Camino desde S hasta c, representado como una lista de nodos.
+camino(S, Res) :-  
+    buscar_camino(S, c, [S], RutaInvertida), 
+    invertir(RutaInvertida, Res),          
+    format('RES = ~w.~n', [Res]),           
+    fail.                                   
+camino(_, _).
 
-camino(S, _Res) :-  
-    findall(Ruta, buscar_camino(S, c, [S], Ruta), Rutas),  
-    maplist(invertir_y_imprimir, Rutas). 
 
+% Predicado que busca un camino desde el nodo S al nodo Dest, evitando nodos ya visitados.
+% - S: Nodo de inicio (actual).
+% - Dest: Nodo de destino.
+% - Visitados: Lista de nodos visitados para evitar ciclos.
+% - Res: Camino encontrado como una lista de nodos en orden inverso.
+buscar_camino(S, S, Cam, Cam).  
+buscar_camino(S, Dest, Visitados, Res) :- 
+    puente(S, P, _),                  
+    \+ member(P, Visitados),         
+    buscar_camino(P, Dest, [P|Visitados], Res).
 
-invertir_y_imprimir(Ruta) :-
-    invertir(Ruta, RutaInvertida),  
-    format('RES = ~w.~n', [RutaInvertida]).  
-
-
+% Invierte una lista de elementos utilizando recursión.
+% - Lista: Lista original que se desea invertir, la divide en [Cabeza|Cola], y la reduce hasta dejarla vacia []
+% - Invertida: Lista resultante con los elementos en orden inverso.
 invertir([], []).  
 invertir([Cabeza|Cola], Invertida) :- 
     invertir(Cola, ColaInvertida), 
     append(ColaInvertida, [Cabeza], Invertida).
 
-
-buscar_camino(S, S, Cam, [S|Cam]).  
-buscar_camino(S, Dest, Visitados, Res) :- 
-    puente(S, P, _),                 
-    \+ member(P, Visitados),         
-    buscar_camino(P, Dest, [P|Visitados], Res).
-
-
+% Encuentra todas las rutas desde S hasta el centro galáctico c, calculando el combustible restante en cada paso.
+% Parámetros:
+% - S: Planeta de inicio.
+% - V: Combustible inicial.
+% - _Res: Variable donde se almacenan los resultados
 combustible(S, V, _Res) :- 
     findall(Ruta, calcular_combustible(S, V, [[S, V]], Ruta), Rutas),
     maplist(imprimir_ruta, Rutas).
 
+
+% Calcula el camino y el combustible restante en cada paso.
+% Parámetros:
+% - S: Nodo actual (planeta).
+% - V: Combustible restante en este punto.
+% - Cam: Camino actual con el combustible restante en cada paso.
+% - Res: Resultado final del camino con los valores de combustible.
 calcular_combustible(c, _, Cam, Cam). 
 calcular_combustible(S, V, Cam, Res) :-
     puente(S, P, W),              
@@ -53,6 +73,11 @@ calcular_combustible(S, V, Cam, Res) :-
     \+ member([P, _], Cam),       
     calcular_combustible(P, VRestante, [[P, VRestante] | Cam], Res).
 
+
+% imprimir_ruta(Ruta)
+% Invierte e imprime una ruta con formato.
+% Parámetros:
+% - Ruta: Ruta a invertir e imprimir.
 imprimir_ruta(Ruta) :-
     reverse(Ruta, RutaInvertida), 
     format('RES = ~w.~n', [RutaInvertida]).
